6b340b66db9014aa6e714be551a5c99d23c12a4c
edwyn mckie <37442282+DarkShinobi100@users.noreply.github.com>
2020-02-10 11:45:10 +0000

    function now prints text using threads

--- taskbased/farm.cpp
+++ taskbased/farm.cpp
@@ -10,24 +10,38 @@ void Farm::add_task(Task *task)
 
 void Farm::run()
 {
+	auto lambda = [=] {
+		while (!TaskQueue.empty()) //while queue is not null
+		{
+			lock_threads.lock();
+			//take a copy of 1st value in the queue
+			Task* task = TaskQueue.front();
+			//remove it from the queue as it's now worked on
+			TaskQueue.pop();
+			lock_threads.unlock();
+			//call the run function from messagetask on that task
+			task->run();
+			//delete our copy
+			delete task;
+		}
+	};
+
 	//std::vector<std::thread> TaskThread;
-	// FIXME - Implement this
 	for (int i = 0; i < std::thread::hardware_concurrency(); i++)
 	{
 		//create thread
 		//add thread to vector
-		TaskThread.push_back(new thread());
+		TaskThread.push_back(new thread(lambda));
 	}
 
-	thread worker_functor([=] {
-		while (!TaskQueue.empty())
-		{
-			lock_threads.lock();
-			Task* task = TaskQueue.front();
-			TaskQueue.pop();
-			lock_threads.unlock();
-			task->run();
-			delete task;
-		}
-	});
+	//includes all variables in local scope by val
+	//thread worker_functor();
+
+	//// Wait for TaskThread to finish.
+	for (int i = 0; i <TaskThread.size(); i++)
+	{
+		TaskThread[i]->join();
+	}		
+
+	//worker_functor.join();
 }
